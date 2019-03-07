cec61186a84803337e1136ecba17d500a5c4435c
vashadow <lbaynum@gmail.com>
2019-03-07 18:34:05 -0500

    .

--- src/pixels_demo.py
+++ src/pixels_demo.py
@@ -7,8 +7,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root():
+    return 'Hello Ferguson Maker Space!'
 
 def flaskThread():
     app.run(host="0.0.0.0", port=80, threaded=True)
